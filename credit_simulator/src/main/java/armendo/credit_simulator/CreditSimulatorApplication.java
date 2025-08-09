7efc1dd8eef68196babb300e950edfe561680a73
Armendo Chandra <mendochandra112@gmail.com>
2025-08-09 21:47:51 +0700

    Menu Load existing json web service

--- credit_simulator/src/main/java/armendo/credit_simulator/CreditSimulatorApplication.java
+++ credit_simulator/src/main/java/armendo/credit_simulator/CreditSimulatorApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.Year;
 import java.util.List;
 import java.util.Scanner;
