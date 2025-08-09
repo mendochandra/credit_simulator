6cff682d51c40e53899a2d992967827837795c71
Armendo Chandra <mendochandra112@gmail.com>
2025-08-09 14:09:01 +0700

    Test commit

--- credit_simulator/src/main/java/armendo/credit_simulator/CreditSimulatorApplication.java
+++ credit_simulator/src/main/java/armendo/credit_simulator/CreditSimulatorApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CreditSimulatorApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(CreditSimulatorApplication.class, args);
 	}
